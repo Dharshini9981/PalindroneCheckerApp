dacfaaf6702c14139ecbf695c414ef4b5a29361e
Dharshini <dharshinikarunakaran7781@gmail.com>
2026-03-03 23:42:28 +0530

    Dharshini added linked list based  Palindrome Checker

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,44 +1,25 @@
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Scanner;
+
 public class PalindroneCheckerApp {
-
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-
-        // Normalize input
-        input = input.replaceAll("\\s+", "").toLowerCase();
-
         Deque<Character> deque = new LinkedList<>();
-
-        // Insert characters into deque
-        for (char ch : input.toCharArray()) {
-            deque.addLast(ch);
+        for (char c : input.toLowerCase().replaceAll("[^a-z]", "").toCharArray()) {
+            deque.addLast(c);
         }
-
         boolean isPalindrome = true;
-
-        // Compare from both ends
         while (deque.size() > 1) {
-
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
         }
-
-        if (isPalindrome) {
-            System.out.println("It is a Palindrome!");
-        } else {
-            System.out.println("It is NOT a Palindrome!");
-        }
-
+        System.out.println("Is palindrome: " + isPalindrome);
         scanner.close();
     }
 }
